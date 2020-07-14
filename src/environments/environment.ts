597ca843ee4d19c8ed78f5f5d7c0d0b0ee527456
Cheng Wu <johnson2006wu@gmail.com>
2020-07-13 22:23:32 -0400

    oshop

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCG34aAvndXzFzgEa4afk4ms5yms-NEhtc",
+    authDomain: "oshop-a1645.firebaseapp.com",
+    databaseURL: "https://oshop-a1645.firebaseio.com",
+    projectId: "oshop-a1645",
+    storageBucket: "oshop-a1645.appspot.com",
+    messagingSenderId: "44861916369",
+    appId: "1:44861916369:web:39a1ef4ea5de04254fb1f0",
+    measurementId: "G-C418Z1SCBV"
+  }
 };
 
 /*
