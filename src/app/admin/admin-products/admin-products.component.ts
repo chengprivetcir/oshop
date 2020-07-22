17d7ff325aad406dc183b443bb68e2881abfc407
Cheng Wu <johnson2006wu@gmail.com>
2020-07-21 20:17:15 -0400

    search

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,19 +1,36 @@
 import { ProductService } from './../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
- products$;
+export class AdminProductsComponent implements OnInit,OnDestroy {
+ products:Product[];
+ subscription:Subscription;
+ filterProducts:Product[];
+
   constructor(private productService:ProductService) {
-    this.products$ = productService.getAll();
+    this.subscription = this.productService.getAll().subscribe(p=>this.filterProducts=this.products=p);
 
    }
 
+   filter(query:string)
+   {
+    this.filterProducts = (query)?
+    this.filterProducts.filter(p=>p.title.toLowerCase().includes(query.toLowerCase())):
+    this.products;
+   
+   }
+
   ngOnInit() {
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
